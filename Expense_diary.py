9a4ec6b4424133d385779ae0022cae7a92e7a093
EugeniyaMo <45200129+EugeniyaMo@users.noreply.github.com>
2018-12-11 00:30:17 +0500

    Add files via upload

--- Expense_diary.py
+++ Expense_diary.py
@@ -8,17 +8,25 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('Expense_diary.ui', self)
+        f = open("data.txt", mode="rb")
+        f.close()
         self.expenceButton.clicked.connect(self.expence)
         self.incomeButton.clicked.connect(self.income)
         self.deleteButton.clicked.connect(self.delete)
 
     def expence(self):
-        #добавить к ОСТАТКУ сумму, введенную в диалоговое окно
-        pass
+        i, okBtnPressed = QInputDialog.getText(
+            self, "Добавить расход", "Введите сумму вашего расхода"
+        )
+        if okBtnPressed:
+            self.expenceButton.setInt(i)
 
     def income(self):
-        #вычесть из ОСТАТКА сумму, введенную в диалоговое окно
-        pass
+        i, okBtnPressed = QInputDialog.getText(
+            self, "Добавить доход", "Введите сумму вашего дохода"
+        )
+        if okBtnPressed:
+            self.incomeButton.setInt(i)
 
     def delete(self):
         #обнулять все значение из файла
